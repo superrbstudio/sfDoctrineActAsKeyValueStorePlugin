c10a04b6d8654738482a41748842a0e228c60e0b
tboutell <tboutell@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2011-04-08 16:05:30 +0000

    Avoid nonunique key issues
    git-svn-id: http://svn.apostrophenow.org/plugins/sfDoctrineActAsKeyValueStorePlugin/trunk@3535 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- test/unit/KeyValueStoreBehaviorTest.php
+++ test/unit/KeyValueStoreBehaviorTest.php
@@ -11,10 +11,12 @@ $t = new lime_test(2);
 
 $user = new sfGuardUser();
 $user->setUsername('fred');
+// Newer sfDoctrineGuardPlugin requires this
+$user->setEmailAddress('fred@test.com');
 $user->setKeyValue('seen_welcome_message', true);
 $user->setKeyValue('colors', array('red', 'green', 'blue'));
 $user->save();
 
-$user = sfGuardUserTable::findOneByUsername('fred');
+$user = sfGuardUserTable::getInstance()->createQuery('u')->where('u.username = ?', 'fred')->fetchOne();
 $t->is($user->getKeyValue('seen_welcome_message'), true, 'seen_welcome_message boolean value successfully retrieved');
 $t->is($user->getKeyValue('colors'), array('red', 'green', 'blue'), 'colors array value successfully retrieved');
